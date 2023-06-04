b642a8449641db11b8ecf08f7aa0a483b5a0acef
papa-Leo <leandroconsani21@gmail.com>
2023-06-04 23:35:40 +0200

    font patcher complete

--- fontConfig.py
+++ fontConfig.py
@@ -6,14 +6,17 @@ def patchFont():
 	# get font path
 	# patch fonts
 	# uninstall tools
-	system('echo INSTALLING FONTFORGE... && sudo apt-get -qq install python3-fontforge -y && echo \bDone')
-	system('cd ~/.ossetup/ && echo DOWNLOADING FONT PATCHER... && wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FontPatcher.zip -q && mkdir fontPatcher && unzip FontPatcher.zip -d fontPatcher/ -q && rm -f FontPatcher.zip && echo Done')
+	system('echo Installing fontforge... && sudo apt install python3-fontforge -y > /dev/null 2>&1 && echo Done')
+	system('cd ~/.ossetup/ && echo Downloading font-patcher... && wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/FontPatcher.zip -q && mkdir fontPatcher && unzip -q FontPatcher.zip -d fontPatcher/ && rm -f FontPatcher.zip && echo Done')
 	system('mkdir ~/.ossetup/fonts/patched')
 	files = os.listdir('/home/papaleo/.ossetup/fonts/')
-	print(files)
 
+	print('Patching font/s; this may take a few minutes...')
 	for file in files:
 		if file.endswith('otf') or file.endswith('ttf'):
-			system('python3 ~/.ossetup/fontPatcher/font-patcher ~/.ossetup/fonts/"' + file + '" -s -c --fontawesome --fontawesomeextension --fontlogos --octicons --codicons --powersymbols --pomicons --powerline --powerlineextra --material --weather -out ~/.ossetup/fonts/patched/')
+			system('python3 ~/.ossetup/fontPatcher/font-patcher ~/.ossetup/fonts/"' + file + '" -s -c -q --fontawesome --fontawesomeextension --fontlogos --octicons --codicons --powersymbols --pomicons --powerline --powerlineextra --material --weather -out ~/.ossetup/fonts/patched/ > /dev/null 2>&1')
+	
+	system('echo Removing font-patcher && cd ~/.ossetup/ && rm -rf fontPatcher/ && sudo apt remove python3-fontforge -y > /dev/null 2>&1')
+	print('Done!')
 
 patchFont()
