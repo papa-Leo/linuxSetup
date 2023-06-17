865fbefa2c3676a6ab300efa6ee6444713337244
papa-Leo <leandroconsani21@gmail.com>
2023-06-17 20:42:15 +0200

    minor bug fixes to pre.py

--- src/pre.py
+++ src/pre.py
@@ -4,10 +4,11 @@ from os.path import exists
 HOME = environ['HOME']
 
 def main():
+	system('sudo apt install python3-pip -y -qqq')
 	system('pip3 install fontTools -qqq')
 
 	if not exists(f'{HOME}/.ossetup'):
-		system(f'mkdir {HOME}/.ossetup/ && cd {HOME}/.ossetup && mkdir fonts/ && mkdir assets/icons/ assets/configFiles/ assets/wallpapers/ assets/archives/')
+		system(f'mkdir {HOME}/.ossetup/ && cd {HOME}/.ossetup && mkdir {HOME}/.ossetup/fonts/ && {HOME}/.ossetup/mkdir {HOME}/.ossetup/assets/icons/ {HOME}/.ossetup/assets/configFiles/ {HOME}/.ossetup/assets/wallpapers/ {HOME}/.ossetup/assets/archives/')
 
 	print('\nPut your prefered monospace font family in the ~/.ossetup/fonts/ directory, then run go.py with sudo.')
 	
