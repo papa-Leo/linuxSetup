7fbe2770b5283bda30c7ecd212f06b57113a150b
papa-Leo <leandroconsani21@gmail.com>
2023-07-01 18:03:48 +0200

    testing directory creation

--- src/pre.py
+++ src/pre.py
@@ -9,6 +9,7 @@ def main():
 	system('pip3 install -qq -r requirements.txt')
 
 	if not exists(f'{HOME}/.ossetup'):
+		system(f'print {HOME}')
 		system(f'mkdir {HOME}/.ossetup/ && cd {HOME}/.ossetup && mkdir {HOME}/.ossetup/fonts/ && mkdir {HOME}/.ossetup/assets/icons/ {HOME}/.ossetup/assets/configFiles/ {HOME}/.ossetup/assets/wallpapers/ {HOME}/.ossetup/assets/archives/')
 
 	print('\nYou can now run go.py')
