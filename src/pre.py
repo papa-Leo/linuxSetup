25e624a6718075efd074891153a5a4acbe7d9e74
papa-Leo <leandroconsani21@gmail.com>
2023-07-01 18:08:48 +0200

    testing directory creation

--- src/pre.py
+++ src/pre.py
@@ -9,8 +9,7 @@ def main():
 	system('pip3 install -qq -r requirements.txt')
 
 	if not exists(f'{HOME}/.ossetup'):
-		system(f'print {HOME}')
-		system(f'mkdir {HOME}/.ossetup/ && cd {HOME}/.ossetup && mkdir {HOME}/.ossetup/fonts/ && mkdir {HOME}/.ossetup/assets/icons/ {HOME}/.ossetup/assets/configFiles/ {HOME}/.ossetup/assets/wallpapers/ {HOME}/.ossetup/assets/archives/')
+		system(f'mkdir {HOME}/.ossetup/ && mkdir {HOME}/.ossetup/fonts/ {HOME}/.ossetup/assets/ {HOME}/.ossetup/assets/icons/ {HOME}/.ossetup/assets/configFiles/ {HOME}/.ossetup/assets/wallpapers/ {HOME}/.ossetup/assets/archives/')
 
 	print('\nYou can now run go.py')
 	# print('\nPut your prefered monospace font family in the ~/.ossetup/fonts/ directory, then run go.py with sudo.')
